2626a07cc71304932e564b291723bfd13979ee4f
Shun Hotta <stars.big2men@gmail.com>
2014-04-14 17:23:21 +0900

    Add validation rules for themes

--- app/models/Theme.php
+++ app/models/Theme.php
@@ -16,4 +16,15 @@ class Theme extends Eloquent {
      */
     protected $hidden = array('user_id');
 
+    /**
+     * The validation rules for store
+     *
+     * @var array
+     */
+    protected $rules = array(
+        ['title' => 'required|min:1'],
+        ['template' => 'required'],
+        ['parent_theme_key' => 'min:40'],
+    );
+
 }
