7f06886e4736c7f0345aa1165cfa82b10f86f61b
Shun Hotta <stars.big2men@gmail.com>
2014-04-14 17:23:03 +0900

    Modify migration

--- app/database/migrations/2014_04_14_121108_create_themes_table.php
+++ app/database/migrations/2014_04_14_121108_create_themes_table.php
@@ -5,37 +5,40 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateThemesTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('themes', function($table)
-		{
-    		$table->increment('id');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('themes', function($table)
+        {
+            $table->increments('id');
 
+            // SHA-1 hashed key for identify
+            $table->string('key', 40)->unique();
             // user_id: 0 for a anonymous user
-    		$table->integer('user_id')->unsigned()->default(0);
-    		$table->string('title');
-    		$table->string('description')->nullable();
-    		$table->text('template');
-    		$table->integer('votes')->unsigned()->default(0);
-    		$table->integer('forks')->unsigned()->default(0);
-    		$table->softDeletes();
-    		$table->timestamps();
-		});
-	}
+            $table->integer('user_id')->unsigned()->default(0);
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->text('template');
+            $table->integer('votes')->unsigned()->default(0);
+            $table->integer('forks')->unsigned()->default(0);
+            $table->string('parent_theme_key', 40)->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('themes');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('themes');
+    }
 
 }
